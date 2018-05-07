ec54ddb48447dd110a1e5ca5a09507f29e4de88c
tiagoporto <tiagoporto@ymail.com>
2018-05-07 15:15:52 -0300

    Add eslint-config-standard and fix lint js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom'
 import App from './pages/App/App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -6,12 +6,12 @@ import './App.styl'
 T.setTexts(require('../../languages/pt-BR.json'))
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{T.translate("welcome")}</h1>
+          <h1 className="App-title">{T.translate('welcome')}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
