6993768b868b08a76cd855c2c13f8a44e374c722
Tiago Porto <tiagoporto@ymail.com>
2018-05-12 21:21:11 -0300

    Change BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 // @flow
 import React, { Component } from 'react'
 import { Provider } from 'mobx-react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 import { Main } from './pages/Main/Main.jsx'
 import Stores from './Stores.js'
 
 export class App extends Component {
   render () {
     return (
-      <BrowserRouter basename="/react-chat">
+      <HashRouter basename="/react-chat">
         <Provider {...Stores}>
           <Switch>
             <Route path="/" component={Main} />
           </Switch>
         </Provider>
-      </BrowserRouter>
+      </HashRouter>
     )
   }
 }
