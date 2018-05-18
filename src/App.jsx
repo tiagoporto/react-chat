bb39f448d3680765a97594a3972b4447065839c1
tiagoporto <tiagoporto@ymail.com>
2018-05-18 16:57:08 -0300

    Add GA

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,16 @@ import { Provider } from 'mobx-react'
 import { HashRouter, Route, Switch } from 'react-router-dom'
 import { Main } from './pages/Main/Main.jsx'
 import Stores from './Stores.js'
+import ReactGA from 'react-ga'
 
 export class App extends Component {
+  componentDidMount () {
+    if (process.env.NODE_ENV !== 'production') {
+      ReactGA.initialize('UA-32351360-4')
+      ReactGA.pageview(window.location.pathname)
+    }
+  }
+
   render () {
     return (
       <HashRouter basename="/">
